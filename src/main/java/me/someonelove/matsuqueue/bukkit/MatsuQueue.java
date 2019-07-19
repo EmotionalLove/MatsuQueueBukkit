0fd05431ca3118c375b62a9965c66b83002738d8
EmotionalLove <sashwho@vivaldi.net>
2019-07-18 21:15:31 -0700

    fix

--- src/main/java/me/someonelove/matsuqueue/bukkit/MatsuQueue.java
+++ src/main/java/me/someonelove/matsuqueue/bukkit/MatsuQueue.java
@@ -22,6 +22,14 @@
  *  * http://www.wtfpl.net/ for more details.
  */
 
+/*
+ *  This program is free software. It comes without any warranty, to
+ *  * the extent permitted by applicable law. You can redistribute it
+ *  * and/or modify it under the terms of the Do What The Fuck You Want
+ *  * To Public License, Version 2, as published by Sam Hocevar. See
+ *  * http://www.wtfpl.net/ for more details.
+ */
+
 /* This program is free software. It comes without any warranty, to
  * the extent permitted by applicable law. You can redistribute it
  * and/or modify it under the terms of the Do What The Fuck You Want
@@ -72,7 +80,6 @@ public final class MatsuQueue extends JavaPlugin implements Listener {
         setGameRule();
         this.getServer().getPluginManager().registerEvents(this, this);
     }
-
 
 
     @Override
